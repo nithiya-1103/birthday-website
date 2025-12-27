feeebdceddb0b3236fadca4f29b4980b66cb7737
Nithiya-B <nithiyabaskaran1103@gmail.com>
2025-12-27 22:48:53 +0530

    Advanced love themed birthday website

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const music = document.getElementById("bgMusic");
-const banner = document.getElementById("soundBanner");
-
-window.onload = () => {
-  if (music) {
-    music.play().catch(() => {
-      if (banner) banner.style.display = "block";
-    });
-  }
-};
-
-function enableSound() {
-  music.play();
-  banner.style.display = "none";
-}
-
 function go(page) {
   window.location.href = page;
+}
+
+// Typing animation
+const text = document.querySelector(".type");
+if (text) {
+  const msg = text.innerText;
+  let i = 0;
+  text.innerText = "";
+  setInterval(() => {
+    if (i < msg.length) {
+      text.innerText += msg.charAt(i);
+      i++;
+    }
+  }, 80);
 }
 
 // Countdown
@@ -23,24 +21,19 @@ const birthday = new Date("2025-03-20").getTime();
 setInterval(() => {
   const t = document.getElementById("timer");
   if (!t) return;
-
-  const now = new Date().getTime();
-  const d = birthday - now;
-
-  if (d < 0) {
-    t.innerHTML = "🎂 Happy Birthday My Love 🎉";
+  const d = birthday - Date.now();
+  if (d <= 0) {
+    t.innerHTML = "🎂 Happy Birthday 🎉";
     return;
   }
-
-  const days = Math.floor(d / 86400000);
-  const hours = Math.floor((d % 86400000) / 3600000);
-  const mins = Math.floor((d % 3600000) / 60000);
-  const secs = Math.floor((d % 60000) / 1000);
-
-  t.innerHTML = `${days}d ${hours}h ${mins}m ${secs}s`;
+  t.innerHTML =
+    Math.floor(d / 86400000) + "d " +
+    Math.floor((d % 86400000) / 3600000) + "h " +
+    Math.floor((d % 3600000) / 60000) + "m " +
+    Math.floor((d % 60000) / 1000) + "s";
 }, 1000);
 
 function showLove() {
-  document.getElementById("loveMessage").innerText =
-    "I choose you. Always. Forever. 💖";
+  document.getElementById("loveText").innerText =
+    "I love you more than words can ever say 💖";
 }
